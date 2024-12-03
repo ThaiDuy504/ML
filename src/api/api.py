41bf65b8d565e3ba3d3ca67e655040c823312404
ThaiDuy504 <thaibuileduythai13@gmail.com>
2024-12-03 17:41:06 +0700

    Refactor FastAPI prediction endpoint to simplify route definition and remove unnecessary model list iteration

--- src/api/api.py
+++ src/api/api.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, File, UploadFile
-from src.model.model import MODEL_LIST
 import src.model.testpredict as testpredict
 import numpy as np
 from skimage.io import imread
@@ -11,15 +10,14 @@ app = FastAPI()
 async def read_root():
     return {"message": "Hello World"}
 
-for model in MODEL_LIST:
-    @app.post(f"/predict/{model}/")
-    async def predict(file: UploadFile = File(...),model=model):
-        with open("temp.jpg", "wb") as buffer:
-            buffer.write(await file.read())
-        image = imread("temp.jpg")
-        [y_hat,classes] = testpredict.predict(image,model)
-        prediction = np.argmax(y_hat)
-        return {"prediction" : f"{classes[prediction]}","all_probability":f"{y_hat.__str__()}"}
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...),model=model):
+    with open("temp.jpg", "wb") as buffer:
+        buffer.write(await file.read())
+    image = imread("temp.jpg")
+    [y_hat,classes] = testpredict.predict(image,model)
+    prediction = np.argmax(y_hat)
+    return {"prediction" : f"{classes[prediction]}","all_probability":f"{y_hat.__str__()}"}
 
 if __name__ == "__main__":
     import uvicorn
