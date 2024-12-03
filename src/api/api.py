8778c716eeed929a1becc1c3e946abe9712b208a
ThaiDuy504 <thaibuileduythai13@gmail.com>
2024-12-03 17:24:20 +0700

    Refactor FastAPI prediction endpoint by removing commented code and cleaning up imports

--- src/api/api.py
+++ src/api/api.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.staticfiles import StaticFiles
 from src.model.model import MODEL_LIST
 import src.model.testpredict as testpredict
 import numpy as np
@@ -12,9 +11,6 @@ app = FastAPI()
 async def read_root():
     return {"message": "Hello World"}
 
-
-
-
 for model in MODEL_LIST:
     @app.post(f"/predict/{model}/")
     async def predict(file: UploadFile = File(...),model=model):
@@ -24,15 +20,6 @@ for model in MODEL_LIST:
         [y_hat,classes] = testpredict.predict(image,model)
         prediction = np.argmax(y_hat)
         return {"prediction" : f"{classes[prediction]}","all_probability":f"{y_hat.__str__()}"}
-
-
-# @app.post("/predict/")
-# async def predict(file: UploadFile = File(...)):
-#     with open("temp.jpg", "wb") as buffer:
-#         buffer.write(await file.read())
-#     image = imread("temp.jpg")
-#     [y_hat,classes] = testpredict.predict(image)
-#     return {"prediction" : f"{classes[int(y_hat[0])]}"}
 
 if __name__ == "__main__":
     import uvicorn
