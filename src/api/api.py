7aca503ae380727b9f1eee730cd3f922647e736e
ThaiDuy504 <thaibuileduythai13@gmail.com>
2024-12-03 17:45:16 +0700

    Refactor FastAPI prediction endpoint to change model parameter handling and ensure temporary file cleanup

--- src/api/api.py
+++ src/api/api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 import src.model.testpredict as testpredict
 import numpy as np
 from skimage.io import imread
+import os
 
 
 app = FastAPI()
@@ -11,12 +12,13 @@ async def read_root():
     return {"message": "Hello World"}
 
 @app.post("/predict/")
-async def predict(file: UploadFile = File(...),model="NONE"):
+async def predict(model, file: UploadFile = File(...)):
     with open("temp.jpg", "wb") as buffer:
         buffer.write(await file.read())
     image = imread("temp.jpg")
     [y_hat,classes] = testpredict.predict(image,model)
     prediction = np.argmax(y_hat)
+    os.remove("temp.jpg")
     return {"prediction" : f"{classes[prediction]}","all_probability":f"{y_hat.__str__()}"}
 
 if __name__ == "__main__":
